10b008c09d8e970f600f223f68ae8d574e3287bb
Kolosowski Arseny <kolosowski.a.r@gmail.com>
2022-09-07 12:44:59 +0300

    Update Package file

--- Package.swift
+++ Package.swift
@@ -4,25 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "ConcurrentDatabase",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ConcurrentDatabase",
-            targets: ["ConcurrentDatabase"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ConcurrentDatabase",
-            dependencies: []),
-        .testTarget(
-            name: "ConcurrentDatabaseTests",
-            dependencies: ["ConcurrentDatabase"]),
-    ]
+	name: "ConcurrentDatabase",
+	products: [
+		.library(
+			name: "ConcurrentDatabase",
+			targets: ["ConcurrentDatabase"]
+		),
+	],
+	dependencies: [
+		.package(
+			url: "https://github.com/realm/realm-swift",
+			from: "10.28.0"
+		)
+	],
+	targets: [
+		.target(
+			name: "ConcurrentDatabase",
+			dependencies: []
+		),
+		.testTarget(
+			name: "ConcurrentDatabaseTests",
+			dependencies: ["ConcurrentDatabase"]
+		),
+	]
 )
