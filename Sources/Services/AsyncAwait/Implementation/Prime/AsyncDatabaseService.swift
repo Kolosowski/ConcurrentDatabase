4cc85802c06b6ba68bf4fd575361ba075dc57665
Kolosowski Arseny <kolosowski.a.r@gmail.com>
2022-09-18 14:18:38 +0300

    Remove useless queue

--- Sources/Services/AsyncAwait/Implementation/Prime/AsyncDatabaseService.swift
+++ Sources/Services/AsyncAwait/Implementation/Prime/AsyncDatabaseService.swift
@@ -3,7 +3,6 @@ import RealmSwift
 
 public final actor AsyncDatabaseService {
 	
-	let workQueue: DispatchQueue = .init(label: "com.databaseService.async.workQueue")
 	let executer: DatabaseExecuter
 	
 	// MARK: - Life Cycle
